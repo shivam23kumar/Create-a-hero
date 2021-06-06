9357228e5c5e1ab8189d5644bdeff0a2e6d0bb01
Shivam Kumar <sk5087323@gmail.com>
2021-06-06 15:17:08 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [name, setName] = useState("")
+  const [Age, setAge] = useState()
+  const [Height, setHeight] = useState()
+  const [SuperPower, setSuperPower] = useState("")
+  const [Displayinfo, setDisplayinfo] = useState(false)
+
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Build a "HERO"</h1>
+      <div>
+        <label>Name:</label>
+        <input type="text" style={{margin:10}} onChange={(event)=>{
+          setName(event.target.value)
+        }}/>
+        <label>Age:</label>
+        <input type="number" style={{margin:10}} onChange={(event)=>{
+          setAge(event.target.value)
+        }}/>
+        <label>Height:</label>
+        <input type="number" style={{margin:10}} onChange={(event)=>{
+          setHeight(event.target.value)
+        }}/>
+        <label>SuperPower:</label>
+        <input type="text" style={{margin:10}} onChange={(event)=>{
+          setSuperPower(event.target.value)
+        }}/>
+      </div>
+      <button onClick={()=>{
+           setDisplayinfo(true);
+      }}>Display Info</button>
+      <div>
+        
+        {Displayinfo && (
+          <div>
+          <h1>Hero Info</h1>
+          <ul>
+            <li>Name: {name}</li>
+            <li>Age: {Age}</li>
+            <li>Height: {Height}</li>
+            <li>SuperPower: {SuperPower}</li>
+          </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
